d53e688d1332e89bbce2c0a89c6596b18c08d289
erik <erik@satellitestud.io>
2020-03-24 11:54:01 +0100

    lint

--- src/TimebarWrapper.container.ts
+++ src/TimebarWrapper.container.ts
@@ -1,21 +1,22 @@
 import { createSelector } from 'reselect'
 import { connect } from 'react-redux'
-import { GeneratorConfig } from "@globalfishingwatch/layer-composer";
+import { GeneratorConfig } from '@globalfishingwatch/layer-composer'
 import TimebarWrapper from './TimebarWrapper'
 import { getStartQuery, getEndQuery } from './model/route.selectors'
 import { updateQueryParams } from './model/router.actions'
-import { FeatureCollection } from 'geojson';
+import { FeatureCollection } from 'geojson'
 
 const getGeneratorConfigs = (state: any) => state.map.generatorConfigs
 const getTracks = (state: any) => state.vessels.tracks
 
-
 const getGeoJSONTracksData = createSelector(
   [getGeneratorConfigs, getTracks],
   (generatorConfigs, tracks) => {
-    const geoJSONTracks: { geojson: FeatureCollection, color:string }[] = []
+    const geoJSONTracks: { geojson: FeatureCollection; color: string }[] = []
     Object.keys(tracks).forEach((id) => {
-      const config:GeneratorConfig = generatorConfigs.find((config:GeneratorConfig) => config.id === id)
+      const config: GeneratorConfig = generatorConfigs.find(
+        (config: GeneratorConfig) => config.id === id
+      )
       if (config !== undefined && config.visible !== false) {
         // TODO I'm not able to export TrackGeneratorConfig
         geoJSONTracks.push({ geojson: tracks[id], color: (config as any).color })
@@ -28,7 +29,7 @@ const getGeoJSONTracksData = createSelector(
 const mapStateToProps = (state: any) => ({
   start: getStartQuery(state),
   end: getEndQuery(state),
-  tracks: getGeoJSONTracksData(state)
+  tracks: getGeoJSONTracksData(state),
 })
 
 const mapDispatchToProps = (dispatch: any) => ({
