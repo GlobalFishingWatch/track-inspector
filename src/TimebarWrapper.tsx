979b8d9b3bf44b85f4f46c7c5fa3b6bc250c95dc
erik <erik@satellitestud.io>
2020-03-31 15:28:08 +0200

    basic graph selector

--- src/TimebarWrapper.tsx
+++ src/TimebarWrapper.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Timebar, { TimebarTracks } from '@globalfishingwatch/map-components/components/timebar'
 import Loader from './Loader'
+import './TimebarWrapper.css'
+
+enum Graph {
+  Encounters = 'Encounters',
+  Speed = 'Speed',
+}
 
 const TimebarWrapper = (props: any) => {
   const { start, end, tracks, setTimerange, loading } = props
+
+  const [currentGraph, setCurrentGraph] = useState(Graph.Speed)
 
   return (
     <div className="timebar">
@@ -22,10 +30,32 @@ const TimebarWrapper = (props: any) => {
           loading ? (
             <Loader />
           ) : (
-            <>{tracks.length && <TimebarTracks key="tracks" {...props} tracks={tracks} />}</>
+            <>
+              {tracks.length && currentGraph === Graph.Encounters && (
+                <TimebarTracks key="tracks" {...props} tracks={tracks} />
+              )}
+              {tracks.length && currentGraph === Graph.Speed && <div></div>}
+            </>
           )
         }
       </Timebar>
+      <div className="graphSelector">
+        <select
+          className="graphSelectorSelect"
+          onChange={(event) => {
+            setCurrentGraph(event.target.value as Graph)
+          }}
+          value={currentGraph}
+        >
+          <option key={Graph.Encounters} value={Graph.Encounters}>
+            {Graph.Encounters}
+          </option>
+          <option key={Graph.Speed} value={Graph.Speed}>
+            {Graph.Speed}
+          </option>
+        </select>
+        <div className="graphSelectorArrow">{/* <Icon icon="graph" /> */}</div>
+      </div>
     </div>
   )
 }
