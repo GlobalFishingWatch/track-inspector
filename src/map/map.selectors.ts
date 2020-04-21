6a6c61529825e28ac172b0b905c290f45fecb8c1
j8seangel <jseangel@gmail.com>
2020-04-21 19:36:16 +0200

    use generic loader selector in map

--- src/map/map.selectors.ts
+++ src/map/map.selectors.ts
@@ -1,14 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit'
 import { Type, GeneratorConfig } from '@globalfishingwatch/layer-composer'
-import { Loader } from '../types'
-import { selectLoaders } from '../loaders/loaders.slice'
 import { selectTracks, selectEvents } from '../vessels/vessels.slice'
 import { selectHighlightedTime } from '../timebar/timebar.slice'
 import { selectDataviewsQuery } from '../routes/routes.selectors'
-
-export const selectMapLoading = createSelector([selectLoaders], (loaders: Loader[]): boolean => {
-  return loaders.filter((l) => l.areas.includes('map')).length > 0
-})
 
 // TODO: deprecate, use GET params
 export const selectGeneratorConfigs = (state: any) => state.map.generatorConfigs
