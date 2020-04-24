2c3f4f171b82838aeaf447e1f9de7423614936b0
erik <erik@satellitestud.io>
2020-04-24 15:35:11 +0200

    zoom buttons

--- src/map/MapControls.jsx
+++ src/map/MapControls.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { updateQueryParams } from '../routes/routes.actions'
+import { selectViewport } from '../routes/routes.selectors'
 import styles from './MapControls.module.css'
+import { ReactComponent as IconPlus } from '../assets/icons/plus.svg'
+import { ReactComponent as IconMinus } from '../assets/icons/minus.svg'
 
 const MapControls = () => {
-  return <div className={styles.mapControls}>lalala</div>
+  const { zoom } = useSelector(selectViewport)
+  const dispatch = useDispatch()
+  return (
+    <div className={styles.mapControls}>
+      <button
+        className={styles.mapControl}
+        onClick={() => {
+          dispatch(updateQueryParams({ zoom: zoom + 1 }))
+        }}
+        aria-label="Increase zoom"
+      >
+        <IconPlus />
+      </button>
+      <button
+        className={styles.mapControl}
+        onClick={() => {
+          dispatch(updateQueryParams({ zoom: zoom - 1 }))
+        }}
+        aria-label="Decrease zoom"
+      >
+        <IconMinus />
+      </button>
+    </div>
+  )
 }
 
 export default MapControls
