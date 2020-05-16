594b6ea7481d3de39a98cd439f4ff421a5c34d65
erik <erik@satellitestud.io>
2020-05-16 13:14:40 +0200

    fixed abs date range

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const EVENTS_COLORS: Dictionary<string> = {
 }
 
 export const TRACK_START = new Date('2017-01-01T00:00:00.000Z')
-export const TRACK_END = new Date('2020-01-01T00:00:00.000Z')
+export const TRACK_END = new Date('2020-05-01T00:00:00.000Z')
 export const TRACK_FIELDS = [Field.lonlat, Field.timestamp, Field.speed]
 
 export const CARRIER_PORTAL_URL = process.env.REACT_APP_CARRIER_PORTAL_URL
--- src/features/timebar/Timebar.tsx
+++ src/features/timebar/Timebar.tsx
@@ -24,6 +24,7 @@ import Loader from 'features/loaders/Loader'
 import { Event, TimebarMode } from 'types/'
 import { selectLoader } from 'features/loaders/loaders.selectors'
 import styles from './Timebar.module.css'
+import { TRACK_START, TRACK_END } from 'config'
 
 const TimebarWrapper = () => {
   const { start, end, dispatchTimerange } = useTimerangeConnect()
@@ -49,8 +50,8 @@ const TimebarWrapper = () => {
         enablePlayback
         start={start}
         end={end}
-        absoluteStart={'2017-01-01T00:00:00.000Z'}
-        absoluteEnd={'2020-01-01T00:00:00.000Z'}
+        absoluteStart={TRACK_START.toISOString()}
+        absoluteEnd={TRACK_END.toISOString()}
         onChange={dispatchTimerange}
         bookmarkStart={bookmarkStart}
         bookmarkEnd={bookmarkEnd}
