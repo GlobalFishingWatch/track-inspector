f2eb87a8b86100a2ba9a344ce1734efb6b8fe5b5
j8seangel <jseangel@gmail.com>
2020-05-21 17:42:29 +0200

    use new timebar component

--- src/types/vendor/@globalfishingwatch/map-components.d.ts
+++ src/types/vendor/@globalfishingwatch/map-components.d.ts
@@ -1,5 +1,4 @@
 declare module '@globalfishingwatch/map-components/components/layer-composer-hook'
-declare module '@globalfishingwatch/map-components/components/timebar'
 declare module '@globalfishingwatch/map-components/components/miniglobe' {
   import { Miniglobe } from '@globalfishingwatch/map-components'
   export = Miniglobe
