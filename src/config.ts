519c6067ad9172206ff5d33a4cbb80fea9b1b802
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-02-11 12:17:36 -0300

    Display pending events in grey on the map

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,7 @@ export const EVENTS_COLORS: Dictionary<string> = {
   encounterunmatched: '#CE2C54',
   loitering: '#cfa9f9',
   port: '#99EEFF',
+  pending: '#AFBBCA',
 }
 
 export const TRACK_START = new Date('2017-01-01T00:00:00.000Z')
